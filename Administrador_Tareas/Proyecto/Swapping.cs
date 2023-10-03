c5d7ea21f38316ff0d18dc525aaeeee9f00b7364
ccambara4 <92342234+ccambara4@users.noreply.github.com>
2023-10-03 12:22:38 -0600

    Cambios

--- Administrador_Tareas/Proyecto/Swapping.cs
+++ Administrador_Tareas/Proyecto/Swapping.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Administrador_Tareas.Proyecto
 {
-    public partial class Swapping : MetroFramework.Forms.MetroForm
+    public partial class Swapping: MetroFramework.Forms.MetroForm
     {
-        private List<char> procesosCargados = new List<char>();
+        private List<string> listaPrincipal = new List<string>();
+        private List<string> listaLetraSeleccionada = new List<string>();
+        private Dictionary<string, IntPtr> memoriaRam = new Dictionary<string, IntPtr>();
+
+        /*No utiliza punteros, pero simula el uso de memoria 
+         * RAM utilizando un diccionario para realizar un 
+         * seguimiento de la memoria asignada y liberada.*/
+
+
         public Swapping()
         {
             InitializeComponent();
-            InicializarComboBox();
+            _ = comboBoxLetra.SelectedIndex;
         }
 
         private void Swapping_Load(object sender, EventArgs e)
@@ -18,73 +27,105 @@ namespace Administrador_Tareas.Proyecto
 
         }
 
-        private void InicializarComboBox()
+        private void btnAgregar_Click(object sender, EventArgs e)
         {
-            // Llena el ComboBox con letras de A a G
-            for (char c = 'A'; c <= 'G'; c++)
+            string letra = comboBoxLetra.SelectedItem.ToString();
+            int numero;
+
+            if (!int.TryParse(textBoxNumero.Text, out numero))
             {
-                comboBoxProcesos.Items.Add(c);
-                comboBoxProcesosCargados.Items.Add(c);
+                MessageBox.Show("Por favor, ingrese un número válido.");
+                return;
+            }
+
+            string elemento = $"{letra} - {numero}";
+            listaPrincipal.Add(elemento);
+
+            // Simular el uso de memoria RAM y obtener la dirección del puntero
+            IntPtr ptr = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(int)));
+            memoriaRam[elemento] = ptr;
+
+            ActualizarListBoxPrincipal();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string letra = comboBoxLetra.SelectedItem.ToString();
+
+            for (int i = listaPrincipal.Count - 1; i >= 0; i--)
+            {
+                string elemento = listaPrincipal[i];
+                if (elemento.StartsWith(letra))
+                {
+                    listaPrincipal.RemoveAt(i);
+                    IntPtr ptr = memoriaRam[elemento];
+                    memoriaRam.Remove(elemento);
+                    Marshal.FreeHGlobal(ptr);
+                }
+            }
+
+            ActualizarListBoxPrincipal();
+        }
+
+        private void btnMoverAListaLetraSeleccionada_Click(object sender, EventArgs e)
+        {
+            string letra = comboBoxLetra.SelectedItem.ToString();
+            int numero;
+
+            if (!int.TryParse(textBoxNumero.Text, out numero))
+            {
+                MessageBox.Show("Por favor, ingrese un número válido.");
+                return;
+            }
+
+            string elemento = $"{letra} - {numero}";
+            listaPrincipal.Remove(elemento);
+            listBoxPrincipal.Items.Remove(elemento);
+
+            listaLetraSeleccionada.Add(elemento);
+            ActualizarListBoxLetraSeleccionada();
+            ActualizarListBoxPrincipal();
+        }
+
+        private void btnDevolverAListaPrincipal_Click(object sender, EventArgs e)
+        {
+            string letra = comboBoxLetraDevolver.SelectedItem.ToString();
+
+            for (int i = listBoxLetraSeleccionada.Items.Count - 1; i >= 0; i--)
+            {
+                string elemento = listBoxLetraSeleccionada.Items[i].ToString();
+                if (elemento.StartsWith(letra))
+                {
+                    listBoxLetraSeleccionada.Items.RemoveAt(i);
+                    listaPrincipal.Add(elemento);
+                    listBoxPrincipal.Items.Add(elemento);
+                }
+            }
+            ActualizarListBoxPrincipal();
+        }
+
+        private void ActualizarListBoxPrincipal()
+        {
+            listBoxPrincipal.Items.Clear();
+            foreach (var elemento in listaPrincipal)
+            {
+                IntPtr ptr = memoriaRam[elemento];
+                listBoxPrincipal.Items.Add($"{elemento} (Memoria RAM: 0x{ptr.ToInt64():X})");
             }
         }
 
-        private void metroLabel1_Click(object sender, EventArgs e)
+        private void ActualizarListBoxLetraSeleccionada()
         {
-
-        }
-
-        private void buttonCargarProceso_Click(object sender, EventArgs e)
-        {
-            char proceso = (char)comboBoxProcesos.SelectedItem;
-            string datosProceso = textBoxDatosProceso.Text;
-            string direccionMemoria = GenerarDireccionMemoria();
-
-            // Agrega el proceso a la lista de procesos cargados
-            procesosCargados.Add(proceso);
-
-            // Muestra el proceso actual en el Label
-            labelProcesoActual.Text = proceso.ToString();
-
-            // Muestra los datos del proceso y la dirección de memoria
-            textBoxDatosProcesoActual.Text = datosProceso;
-            textBoxDireccionMemoria.Text = direccionMemoria;
-
-            // Actualiza los TextBox de procesos cargados
-            ActualizarTextBoxProcesosCargados();
-
-            // Limpia los controles para el próximo proceso
-            comboBoxProcesos.SelectedIndex = -1;
-            textBoxDatosProceso.Clear();
-        }
-
-        private void ActualizarTextBoxProcesosCargados()
-        {
-            for (int i = 0; i < procesosCargados.Count && i < 5; i++)
+            listBoxLetraSeleccionada.Items.Clear();
+            foreach (var letra in listaLetraSeleccionada)
             {
-                TextBox textBox = Controls.Find($"textBoxProceso{i + 1}", true)[0] as TextBox;
-                textBox.Text = procesosCargados[i].ToString();
+                listBoxLetraSeleccionada.Items.Add(letra);
             }
         }
 
-        private string GenerarDireccionMemoria()
+        private void btnCerrar_Click(object sender, EventArgs e)
         {
-            // Simplemente genera una dirección de memoria aleatoria
-            Random random = new Random();
-            return $"0x{random.Next(0x1000, 0xFFFF):X4}";
-        }
-
-        private void metroButton2_Click(object sender, EventArgs e)
-        {
-            char procesoSeleccionado = (char)comboBoxProcesosCargados.SelectedItem;
-            procesosCargados.Remove(procesoSeleccionado);
-
-            // Actualiza los TextBox de procesos cargados
-            ActualizarTextBoxProcesosCargados();
-
-            // Limpia los controles relacionados con el proceso eliminado
-            labelProcesoActual.Text = "";
-            textBoxDatosProcesoActual.Clear();
-            textBoxDireccionMemoria.Clear();
+            Close();
         }
     }
 }
